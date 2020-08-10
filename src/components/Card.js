3dcf70dfcde969d1c2c396ca2947adde0eb111f4
Nico <skulls.ad@gmail.com>
2020-08-10 22:12:39 +0200

    Added error broundy.

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,8 @@ const Card = (props) => {
 	const imgUrl = `https://robohash.org/${id}?size=200x200&set=set3`;
 
 	return (
-		<div className='tc bg-light-yellow dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img src={imgUrl} alt={name}/> 
+		<div className='tc dib br3 pa3 ma2 grow bw2 shadow-5 card'>
+			<img src={imgUrl} alt={name} />
 			<h2>{name}</h2>
 			<p>{email}</p>
 		</div>
