debc9c97c958ca196619652faab5ed24c3731334
Nico <skulls.ad@gmail.com>
2020-08-10 22:23:21 +0200

    Updates

---
+++ precache-manifest.e8ee08418b75121ee6ef31d132385700.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ee8c8cfd3a993d3d014a8ed4ef0a46",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "47e1933332c207c8649e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3949607f2121cf0c0312",
+    "url": "/robofriends/static/css/main.ed4e89b5.chunk.css"
+  },
+  {
+    "revision": "47e1933332c207c8649e",
+    "url": "/robofriends/static/js/2.58523ca3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.58523ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3949607f2121cf0c0312",
+    "url": "/robofriends/static/js/main.d062b20a.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
